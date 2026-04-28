9eb6ba2f7160bbb590603944eee7d9e5d6fea61f
jpmoo <moore.jpaul@gmail.com>
2026-04-28 18:38:07 -0400

    Initial Commit

---
+++ notch/notchApp.swift
@@ -0,0 +1,17 @@
+//
+//  notchApp.swift
+//  notch
+//
+//  Created by J. P. Moore on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct notchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
